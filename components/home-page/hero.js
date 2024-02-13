a664f885f861c5d68e1a7a1ca2283791226dc7b2
MirkoCvetanoski <mirko.cvetanoski@gmail.com>
2024-02-13 11:27:03 +0100

    Finished project

---
+++ components/home-page/hero.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/web-developer.png"
+          alt="An image of Kiyoaki"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Kiyoaki</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
